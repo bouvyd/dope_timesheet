e028ada41521d0b59d7ac2c271932564c5693b54
Damien Bouvy <dbo@odoo.com>
2024-09-23 20:53:03 +0200

    init app, minimal theme and plumbing

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                caveat: ['Caveat', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                purple: '#714b67',
+                blue: '#1bb6f9',
+                orange: '#fbb130',
+                red: '#fc787d',
+                green: '#00ceb3',
+            },
+        },
+    },
+    plugins: [],
+}
